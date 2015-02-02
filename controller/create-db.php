f0efd7217c95bbb94472b40bba40de87ea88d1bb
Kiera <JustForLaughts66@gmail.com>
2015-02-02 10:45:28 -0800

    on video 15.

--- controller/create-db.php
+++ controller/create-db.php
@@ -15,7 +15,7 @@
     $exists = $connection->select_db($database); 
       
     //Remember ! means it does not.  
-    //Queries are questions to the data base.in this case we are sending commands. 
+    //Queries are questions to the data base in this case we are sending commands. 
     if(!$exists) {
         $query = $connection->query("CREATE DATABASE $database"); 
           
@@ -23,6 +23,27 @@
         if($query) {
             echo "Successfully created database: " . $database;
         }
+    } 
+    //Since the code will only run when the database doesn't exist, it will only run once if the database was sucessfully created. 
+    //This else statement will only apply if the database already exists.
+    else {
+        echo "Database already exists.";
+    } 
+    //The name of this table is posts which is a table that stores all the posts on the blog. 
+    //NOT NULL means the id can never be empty. AUTO_INCREMENT will give each post a unique id automatically. 
+    //255 will be the maxium number of charaters within the title.
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT," 
+            . "title varchar(255) NOT NULL," 
+            . "post text NOT NULL," 
+            . "PRIMARY KEY (id))");  
+     
+    if($query) {
+        echo "Successfully created table: posts";
+    } 
+    else {
+        echo "$connection->error";
     }
+    
    
     $connection->close();
