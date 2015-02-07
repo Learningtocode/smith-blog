21f882c20f7497d276673345ded91cc577fcba1a
Kiera <JustForLaughts66@gmail.com>
2015-02-06 23:01:54 -0800

    I just finished video 23.

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,16 @@
-<?php
-    $title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); 
+<?php 
+    require_once(__DIR__ . "/../model/database.php"); 
+     
+    //Mysqli is the object that will connect us to the database. 
+    //The variables in the parenthesis are four parameters.
+    $connection = new mysqli($host, $username, $password, $database);
+    
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); 
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING); 
     
     echo "<p>Title: $title</p>"; 
-    echo "<p>Post: $post</p>";
+    echo "<p>Post: $post</p>"; 
+     
+    //You must remember to close your connection. 
+    $connection->close();
+    
